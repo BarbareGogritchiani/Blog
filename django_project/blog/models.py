f7dfbc447108adc8774e4545e9a1d97aabb4a6ee
BarbareGogritchiani <gogrichianibarbare@gmail.com>
2025-01-13 03:17:16 +0400

    Database and Migrations

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #onetomany, if user is deleted all post will be deleted
+
+    def __str__(self):
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author' : 'Barbare',
-        'title' : 'BlogPost 1',
-        'content' : 'First Post',
-        'date_posted' : 'January 8, 2024'
-    },
-    {
-        'author': 'Nino',
-        'title': 'BlogPost 2',
-        'content': 'Second Post',
-        'date_posted': 'January 9, 2024'
-    },
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
